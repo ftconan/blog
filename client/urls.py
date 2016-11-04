46d3b706253157eaba5b4b3ac27db91cf503a2d0
冯涛 <1508907793@qq.com>
2016-11-04 20:44:14 +0800

    建立blog数据库，后台添加博文界面和博文列表界面完成

---
+++ client/urls.py
@@ -0,0 +1,9 @@
+# coding:utf-8
+
+from django.conf.urls import url, include
+
+import views
+
+urlpatterns = [
+	url(r'^$', views.index),
+]
---
+++ cms/urls.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+from django.conf.urls import url
+import views
+import load
+
+urlpatterns = [
+	#登录界面
+	url(r'^login$', views.login),
+	# url(r'^logout$', views.logout),
+
+	#仪表盘
+	url(r'^dashboard', load.dashboard),
+
+	#发布博文
+	url(r'^addNews', load.addNews),
+	url(r'^newsList', load.newsList),
+]
