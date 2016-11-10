d7eeffc0ba50160699a114223a9fa40179186799
冯涛 <1508907793@qq.com>
2016-11-10 19:20:01 +0800

    添加前台一级和二级界面

--- client/views.py
+++ client/views.py
@@ -17,6 +17,46 @@ def blog(req):
 	"""
 	return render(req, "first/blog.html")
 
+def album(req):
+	"""
+	相册
+	:param req:
+	:return:
+	"""
+	return render(req, "first/album.html")
+
+def music(req):
+	"""
+	音乐
+	:param req:
+	:return:
+	"""
+	return render(req, "first/music.html")
+
+def film(req):
+	"""
+	视频
+	:param req:
+	:return:
+	"""
+	return render(req, "first/film.html")
+
+def share(req):
+	"""
+	分享
+	:param req:
+	:return:
+	"""
+	return render(req, "first/share.html")
+
+def message(req):
+	"""
+	留言板
+	:param req:
+	:return:
+	"""
+	return render(req, "first/message.html")
+
 def secondBlog(req):
 	"""
 	博文二级页
@@ -24,3 +64,29 @@ def secondBlog(req):
 	:return:
 	"""
 	return render(req, "second/secondBlog.html")
+
+def secondAlbum(req):
+	"""
+	相册二级页
+	:param req:
+	:return:
+	"""
+	return render(req, "second/secondAlbum.html")
+
+def secondMusic(req):
+	"""
+	音乐二级页
+	:param req:
+	:return:
+	"""
+	return render(req, "second/secondMusic.html")
+
+def secondFilm(req):
+	"""
+	视频二级页
+	:param req:
+	:return:
+	"""
+	return render(req, "second/secondFilm.html")
+
+
