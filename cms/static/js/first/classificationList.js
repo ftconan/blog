7524728b4f7c13f9c0d95570c38bca608e2b212b
冯涛 <1508907793@qq.com>
2016-12-05 21:55:41 +0800

    栏目列表显示数据和删除功能实现

--- cms/static/js/first/classificationList.js
+++ cms/static/js/first/classificationList.js
@@ -1,3 +1,38 @@
-/**
- * Created by Administrator on 2016/11/14.
- */
+$(function(){
+    classificationList();     //加载栏目列表
+});
+
+//加载栏目列表
+function classificationList(){
+    $.post("getClassificationList",function(data){
+       var jsonData = $.parseJSON(data);
+       var str = "";
+       for(var i in jsonData){
+           str+= "<tr><td>"+jsonData[i].classificationIdLevel+"</td>";
+           str+="<td>"+jsonData[i].classificationName+"</td>";
+           str+="<td>"+jsonData[i].parentDirectory+"</td>";
+           str+="<td>"+jsonData[i].classificationOrder+"</td>";
+           str+="<td>"+jsonData[i].isShow+"</td>";
+           str+="<td><a class='edit' href='/cms/addClassification?update="+jsonData[i].classificationId+"' data-toggle='modal' id='"+jsonData[i].classificationId+"'><i class='fa fa-edit' title='修改'></i> 修改</a> <a class='delete' href='#' data-toggle='modal' id='"+jsonData[i].classificationId+"'><i class='fa fa-times' title='删除'></i> 删除</a></td></tr>";
+       }
+       $(".classificationList").empty().append(str);
+    });
+}
+
+//删除栏目
+$("body").on("click",".delete",function(data){
+   var classificationId = $(this).attr("id");
+   if(confirm("确定要删除该条数据?")){
+       $.post("deleteClassification",{"classificationId": classificationId},function(data){
+           if(data){
+               alert("删除成功!");
+               location.reload(true);
+           }
+           else{
+               alert("删除失败!");
+               return false;
+           }
+       });
+   }
+   classificationList();
+});
